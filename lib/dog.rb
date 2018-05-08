449fa35f7c13a28b8fb7945c3d598f589aac6469
Christiana Vandermale <christianavdm@gmail.com>
2018-05-08 02:33:22 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,11 +8,11 @@ class Dog
     @@all << name
   end
   
-  def self.clear_all
+  self.clear_all
     @@all = []
   end
   
-  def self.all
+  self.all
     @@all.each do |name|
       puts name
     end
