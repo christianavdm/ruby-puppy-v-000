29fcaee9c7b4a281dad4451b97e2c2547392c854
Christiana Vandermale <christianavdm@gmail.com>
2018-05-08 02:36:23 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,12 @@
 require 'pry'
 class Dog 
+
+  attr_accessor :name
   @@all = [] 
    
   def initialize(name)
     @name = name
-    @@all << self
+    @@all << name
   end
   
   def self.clear_all
